7e48cb6e39a6f871b12dc39327639c97792fcead
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-23 13:38:52 +0530

    add scroll indicator component with data fetching and progress tracking
    - Created App component to render ScrollIndicator with a sample API URL.
    - Implemented ScrollIndicator component to fetch product data and display scroll progress.
    - Added CSS styles for various components including scroll progress bar and layout.
    - Included assets: hero image and SVG logos for React and Vite.
    - Configured Vite for React application.

---
+++ scrollIndicator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScrollIndicator from './components'
+
+const App = () => {
+  return (
+    <div>
+      <ScrollIndicator  url={'https://dummyjson.com/products?limit=100'}/>
+    </div>
+  )
+}
+
+export default App
